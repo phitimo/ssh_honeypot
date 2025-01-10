1681c4b0832a8eca7b791911a50ed67e5ef109b2
phitimo <timo.philipp314@gmail.com>
2025-01-10 23:19:02 +0100

    code comments

--- spark/spark-speed-layer.py
+++ spark/spark-speed-layer.py
@@ -2,11 +2,12 @@ from pyspark.sql import SparkSession
 from pyspark.sql.functions import *
 from pyspark.sql.types import *
 
+# Funktion zum Schreiben in MariaDB. Wird für foreachBatch() benötigt. Streaming-DataFrames können nicht direkt in MariaDB geschrieben werden, sie müssen in Batches geschrieben werden.
 def write_to_mariadb(batch_df, batch_id, table_name):
     batch_df.write.jdbc(url='jdbc:mariadb://mariadb:3306/db', table=table_name, properties={'user': 'bde', 'password': 'bde'}, mode="append")
     
     
-# Spark-Session starten
+# Spark-Session starten. Paket für Kafka-Integration hinzufügen mittels config("spark.jars.packages", "org.apache.spark:spark-sql-kafka-0-10_2.12:3.5.0")
 spark = SparkSession.builder \
     .appName("SSH-Honeypot-Analytics") \
     .config("spark.jars.packages", "org.apache.spark:spark-sql-kafka-0-10_2.12:3.5.0") \
@@ -23,10 +24,12 @@ schema = StructType([
     StructField("success", BooleanType(), True)
 ])
 
+# Kafka-Cluster und Topic definieren
 kafka_broker = "kafka-cluster:9092,kafka-cluster.default.svc.cluster.local:9020"
+# Subscriben auf Topic "ssh-logs"
 kafka_topic = "ssh-logs"
 
-# Kafka-Stream lesen
+# Kafka-Stream lesen. SASL-Authentifizierung mit Benutzername und Passwort. Passwort und Username müssen in der Kafka-Config-Map hinterlegt werden. Aus einfachheitsgründen sind sie hier hardcoded.
 df = spark.readStream \
     .format("kafka") \
     .option("kafka.bootstrap.servers", kafka_broker) \
@@ -38,6 +41,7 @@ df = spark.readStream \
     .load()
 
 
+# JSON aus Kafka parsen und in MariaDB schreiben. Zwei Streams: Einmal alle Login-Versuche und einmal nur die aus Deutschland.
 parsed_df = df.select(from_json(col("value").cast("string"), schema).alias("data")).select("data.*")
        
 query1 =  parsed_df.writeStream.foreachBatch(lambda batch_df, batch_id: write_to_mariadb(batch_df, batch_id, "login_attempts")).outputMode("append").option("sql.quote.identifier", "`").start()
@@ -45,7 +49,8 @@ query1 =  parsed_df.writeStream.foreachBatch(lambda batch_df, batch_id: write_to
 attempts_germany = parsed_df.filter(col("country") == "Germany")
 
 query2 = attempts_germany.writeStream.foreachBatch(lambda batch_df, batch_id: write_to_mariadb(batch_df, batch_id, "login_attempts_germany")).outputMode("append").option("sql.quote.identifier", "`").start()
-       
+
+# Warten, bis die Streams beendet werden
 query1.awaitTermination()
 query2.awaitTermination()
 
